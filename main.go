25316d0c732c84c2d4e4428e1c23dee0ffcce567
Shreya Ganguly <shreya.ganguly99@gmail.com>
2017-08-25 21:27:40 +0530

    Added tablimit so that new window open after configurable number of terminal tabs

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -18,21 +20,52 @@ var (
 	commands []string
 )
 
+const (
+	tabLimit = 10
+)
+
 func main() {
 	flag.Parse()
+	var newWindow bool
 	readCommands()
 	for _, v := range commands {
-		cmd := exec.Command("osascript", "-e", "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down", "-e", fmt.Sprintf("tell application \"Terminal\" to do script \"%s\" in front window", v))
-		var out bytes.Buffer
-		var stderr bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Stderr = &stderr
-		err := cmd.Run()
-		if err != nil {
-			fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-			return
+		var i int
+		var err error
+		if newWindow == false {
+			cmd := exec.Command("osascript", "-e", "tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down", "-e", fmt.Sprintf("tell application \"Terminal\" to do script \"%s\" in front window", v))
+			var out bytes.Buffer
+			var stderr bytes.Buffer
+			cmd.Stdout = &out
+			cmd.Stderr = &stderr
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+				return
+			}
+			i, err = strconv.Atoi(strings.Split(out.String(), " ")[1])
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+		} else {
+			cmd := exec.Command("osascript", "-e", fmt.Sprintf("tell application \"Terminal\" to do script \"%s\"", v))
+			var out bytes.Buffer
+			var stderr bytes.Buffer
+			cmd.Stdout = &out
+			cmd.Stderr = &stderr
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+				return
+			}
+			newWindow = false
+
 		}
-		fmt.Println("Result: " + out.String())
+
+		if i > tabLimit {
+			newWindow = true
+		}
 	}
 
 }
